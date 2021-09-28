e949f817cbeba4a34ff11860e70815f2e4c0d014
2515299122 <2515299122@qq.com>
2021-09-28 12:44:33 +0800

    二叉树的层序遍历

--- src/base/tree/BinaryTree.java
+++ src/base/tree/BinaryTree.java
@@ -1,5 +1,8 @@
 package base.tree;
 
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class BinaryTree {
@@ -29,7 +32,7 @@ public class BinaryTree {
 
     public void preOrder(TNode root) {
         if (root != null) {
-            System.out.print(root.data + " ");
+            System.out.print(root.data);
             preOrder(root.lchild);
             preOrder(root.rchild);
         }
@@ -38,7 +41,7 @@ public class BinaryTree {
     public void inOrder(TNode root) {
         if (root != null) {
             inOrder(root.lchild);
-            System.out.print(root.data + " ");
+            System.out.print(root.data);
             inOrder(root.rchild);
         }
     }
@@ -47,18 +50,18 @@ public class BinaryTree {
         if (root != null) {
             postOrder(root.lchild);
             postOrder(root.rchild);
-            System.out.print(root.data + " ");
+            System.out.print(root.data);
         }
     }
 
     public void preOrderByRecursion(TNode root) {
-        if(root==null) return;
+        if (isEmpty()) return;
         Stack<TNode> stack = new Stack<>();
         TNode node;
         stack.push(root);
         while (!stack.isEmpty()) {
             node = stack.pop();
-            System.out.print(node.data + " ");
+            System.out.print(node.data);
             if (node.rchild != null) {
                 stack.push(node.rchild);
             }
@@ -78,38 +81,62 @@ public class BinaryTree {
             }
             if (!stack.isEmpty()) {
                 node = stack.pop();
-                System.out.print(node.data + " ");
+                System.out.print(node.data);
                 node = node.rchild;
             }
         }
     }
 
     public void postOrderByRecursion(TNode root) {
-        if(root==null) return;
+        if (isEmpty()) return;
         Stack<TNode> s1 = new Stack<>();
         Stack<Character> s2 = new Stack<>();
         TNode node;
         s1.push(root);
-        while (!s1.isEmpty()){
-            node=s1.pop();
+        while (!s1.isEmpty()) {
+            node = s1.pop();
             s2.push(node.data);
-            if(node.lchild!=null){
+            if (node.lchild != null) {
                 s1.push(node.lchild);
             }
-            if(node.rchild!=null){
+            if (node.rchild != null) {
                 s1.push(node.rchild);
             }
         }
-        while (!s2.isEmpty()){
-            System.out.print(s2.pop()+" ");
+        while (!s2.isEmpty()) {
+            System.out.print(s2.pop());
         }
     }
 
-        public static void main(String[] args) {
-       /* A B D   G     E   H     C   F
-          D   G   B   E   H   A   C   F
-          G D       H E B       F C A
-        */
+    public void levelOrder() {
+        if (isEmpty()) return;
+        TNode node;
+        Queue<TNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+            System.out.print(node.data);
+            if (node.lchild != null) {
+                queue.offer(node.lchild);
+            }
+            if (node.rchild != null) {
+                queue.offer(node.rchild);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        /*ABD G  E H  C F
+         * D G B E H A C F
+         *   GD   HEB   FCA
+         * ABCDE F G H
+         */
+        /*                  A
+        *              B         C
+        *           D     E     #  F
+        *          # G   # #      # H
+        *           # #
+        * */
         BinaryTree binaryTree = new BinaryTree();
         binaryTree.createTreeByRecursion("ABD#G##E#H##C#F##".toCharArray());
         binaryTree.preOrder(binaryTree.root);
@@ -123,6 +150,8 @@ public class BinaryTree {
         binaryTree.inOrderByRecursion(binaryTree.root);
         System.out.println();
         binaryTree.postOrderByRecursion(binaryTree.root);
+        System.out.print("\n================================\n");
+        binaryTree.levelOrder();
     }
 
     class TNode {
