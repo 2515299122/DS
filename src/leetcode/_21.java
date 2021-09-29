a13ca3632ad9b5c27b69c4d119be584d3c3d555d
2515299122 <2515299122@qq.com>
2021-09-29 10:20:04 +0800

    21.合并两个有序链表

---
+++ src/leetcode/_21.java
@@ -0,0 +1,80 @@
+package leetcode;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// @solution-sync:begin
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode p = dummy, p1 = l1, p2 = l2;
+        while (p1 != null && p2 != null) {
+            if (p1.val <= p2.val) {
+                p.next = p1;
+                p1 = p1.next;
+            } else {
+                p.next = p2;
+                p2 = p2.next;
+            }
+            p = p.next;
+        }
+        if (p1 == null) p.next = p2;
+        if (p2 == null) p.next = p1;
+        return dummy.next;
+    }
+}
+// @solution-sync:end
+
+class Main {
+
+    public static void main(String[] args) {
+        ListNode l1 = parseListNode(new int[]{1, 2, 4});
+        ListNode l2 = parseListNode(new int[]{1, 3, 4});
+
+        ListNode result = new Solution().mergeTwoLists(l1, l2);
+        System.out.println(toString(result));
+    }
+
+    private static ListNode parseListNode(int[] values) {
+        ListNode head = null;
+        ListNode tail = null;
+        for (int value : values) {
+            ListNode node = new ListNode(value);
+            if (head == null)
+                head = node;
+            else
+                tail.next = node;
+            tail = node;
+        }
+        return head;
+    }
+
+    private static String toString(ListNode h) {
+        StringBuilder buf = new StringBuilder();
+        buf.append("[");
+        ListNode c = h;
+        while (c != null) {
+            buf.append(c.val);
+            if (c.next != null)
+                buf.append(",");
+            c = c.next;
+        }
+        buf.append("]");
+        return buf.toString();
+    }
+
+}
