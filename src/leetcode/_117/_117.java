4772da6810da3c9a11c6c0cd2f93255f3303cd34
2515299122 <2515299122@qq.com>
2021-09-30 12:08:20 +0800

    117.填充每个节点的下一个右侧节点指针2

---
+++ src/leetcode/_117/_117.java
@@ -0,0 +1,48 @@
+package leetcode._117;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) return null;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            Node pre = null;
+            int leafCount = queue.size();
+            for (int i = 0; i < leafCount; i++) {
+                Node node = queue.poll();
+                if (pre != null) {
+                    pre.next = node;
+                }
+                pre = node;
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+}
